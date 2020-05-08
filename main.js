e37d30f078052b4a2012fe1f996830b2c736a77f
Jinming Wu, Patrick <me@patrickwu.space>
2020-05-08 10:24:10 +0800

    remove unnecessary ref

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 const electron = require('electron')
-const open = require('open');
-const ipc = require('electron').ipcMain;
 // Module to control application life.
 const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
-const path = require('path')
-const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
