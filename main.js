97ca99be0d29e054fd2d49073c8827d528a53328
Jinming Wu, Patrick <me@patrickwu.space>
2022-12-23 18:14:12 +0800

    clean up general code and do additional styling

--- main.js
+++ main.js
@@ -1,56 +1,74 @@
-const electron = require('electron')
+const electron = require("electron");
 // Module to control application life.
-const app = electron.app
+const app = electron.app;
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const BrowserWindow = electron.BrowserWindow;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600,icon:__dirname + '/icon.jpg', frame: true})
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: __dirname + "/icon.jpg",
+    frame: true,
+  });
 
   // and load the index.html of the app.
-  mainWindow.loadURL("https://todo.microsoft.com/?app")
-  
+  mainWindow.loadURL("https://todo.microsoft.com/?app");
+
   // Open the DevTools.
-  //mainWindow.webContents.openDevTools()
-  mainWindow.webContents.on('did-finish-load', function() {
-    mainWindow.webContents.insertCSS('.o365sx-appName, #ShellAboutMe, #ShellSettings, #O365_HeaderLeftRegion, #todoWhatsNewBtn{ display: none !important;}')
+  mainWindow.webContents.openDevTools();
+
+  var css = `
+  .o365sx-appName,
+  #ShellAboutMe,
+  #ShellSettings,
+  #O365_HeaderLeftRegion,
+  #todoWhatsNewBtn,
+  .officeApps {
+    display: none !important;
+  }
+  
+  #toDoSearchBox {
+    padding: 0 10px;
+  }
+`;
+  mainWindow.webContents.on("did-finish-load", function () {
+    mainWindow.webContents.insertCSS(css);
   });
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  mainWindow.on("closed", function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    mainWindow = null;
+  });
 }
-app.on('browser-window-created',function(e,window) {
+app.on("browser-window-created", function (e, window) {
   window.setMenu(null);
 });
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on("window-all-closed", function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-app.on('activate', function () {
+app.on("activate", function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
-
-
+});
